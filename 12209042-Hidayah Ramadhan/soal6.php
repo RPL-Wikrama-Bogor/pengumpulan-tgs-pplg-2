1b02f34cf782541a76d4fd90f696e7a8ee990a54
HidayahRamadhan10 <hidayahramadhan@smkwikrama.sch.id>
2023-08-24 10:14:28 +0700

    penambah kodingan

---
+++ 12209042-Hidayah Ramadhan/soal6.php
@@ -0,0 +1,49 @@
+<?php
+// Fungsi untuk mengkonversi total detik ke format jam-menit-detik
+// floor : membulatkan kebawah
+// ceil : membulatkan keatas 
+// round : membulatkan keatas dan kebawah
+function konversiDetikKeWaktu($totalDetik) {
+    $jam = floor($totalDetik / 3600);
+    $sisaDetik = $totalDetik % 3600;
+    $menit = floor($sisaDetik / 60);
+    $detik = $sisaDetik % 60;
+
+    return "$jam jam $menit menit $detik detik";
+}
+
+// Inisialisasi variabel
+$totalDetik = 0;
+$hasilKonversi = '';
+
+// Memeriksa apakah ada input dari user
+if (isset($_POST['submit'])) {
+    $totalDetik = $_POST['total_detik'];
+    // $_POST untuk memanggil data yang telah diinputkan 
+
+    // Memanggil fungsi konversiDetikKeWaktu() untuk mengkonversi total detik
+    $hasilKonversi = konversiDetikKeWaktu($totalDetik);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Detik ke Waktu</title>
+</head>
+<body>
+    <h2>Detik ke Waktu</h2>
+    <form method="post" action="">
+        Masukkan total detik: <input type="number" name="total_detik" value="<?php echo $totalDetik; ?>" required>
+        <input type="submit" name="submit" value="Konversi">
+    </form>
+
+    <?php
+    // Menampilkan hasil konversi jika sudah ada input
+    if ($hasilKonversi !== '') {
+         // Melakukan sesuatu jika $hasilKonversi tidak identik dengan string kosong ('')
+        echo "<p>Hasil konversi: $hasilKonversi</p>";
+    }
+    ?>
+</body>
+</html>
