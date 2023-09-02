6672c8dbf0a60f8495c57f3eea59c8b48924d73e
hikmatunnisarangkuti11 <hikmatunnisarangkuti@smkwikrama.sch.id>
2023-09-02 18:12:43 +0700

    merubah file

--- 12209044-Hikmatunnisa Rangkuti/soal18.php
+++ 12209044-Hikmatunnisa Rangkuti/soal18.php
@@ -1,36 +1,75 @@
-<?php
-    $bilangan;
-    $satuan;
-    $puluhan;
-    $ratusan;
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bilangan bulat</title>
+    <title>Mencari Rata-rata Nilai Tertinggi</title>
 </head>
 <body>
-    <form action="" method="post">
-        <div style="display: flex;">
-            <label for="bil">Bilangan :</label>
-            <input type="number" name="bil" id="bil">
-        </div>
-        <button type="submit" name="submit">Kirim!</button>
-    </form>
+    <?php
+    session_start();
+
+    if (!isset($_SESSION['loop'])) {
+        $_SESSION['loop'] = 0; 
+        $_SESSION['tertinggi'] = null; 
+        $_SESSION['juara'] = null;
+    }
+
+    if ($_SESSION['loop'] >= 15) {
+        session_destroy();
+        echo "<p>Anda telah mencapai batas 10 kali input.</p>";
+    } else {
+        ?>
+        <form method="post" action="">
+            <label>Nama Siswa:</label><br>
+            <input type="text" name="nama" class="form-control">
+            <br>
+            <label> Matematika:</label><br>
+            <input type="number" name="matematika" class="form-control">
+            <br>
+            <label> Bahasa Indonesia:</label><br>
+            <input type="number" name="indonesia" class="form-control">
+            <br>
+            <label> Bahasa Inggris:</label><br>
+            <input type="number" name="inggris" class="form-control">
+            <br>
+            <label> DPK:</label><br>
+            <input type="number" name="dpk" class="form-control">
+            <br>
+            <label> PABP:</label><br>
+            <input type="number" name="agama" class="form-control">
+            <br>
+            <input type="submit" value="Cari Tertinggi" class="btn btn-primary">
+        </form>
+      </div>
+    </div>
+</center>
+<div class="card" style="width:23rem; padding:3rem; top: -100px;" name="kotak" >
+        <?php
+
+        if(isset($_POST['nama']) && isset($_POST['matematika']) && isset($_POST['indonesia']) && isset($_POST['inggris']) && isset($_POST['dpk']) && isset($_POST['agama'])) {
+            $nama = $_POST['nama'];
+            $matematika = $_POST['matematika'];
+            $indonesia = $_POST['indonesia'];
+            $inggris = $_POST['inggris'];
+            $dpk = $_POST['dpk'];
+            $agama = $_POST['agama'];
+
+         
+            $rata = ($matematika + $indonesia + $inggris + $dpk + $agama) / 5;
+
+            if (!isset($_SESSION['tertinggi']) || $rata > $_SESSION['tertinggi']) {
+                $_SESSION['tertinggi'] = $rata;
+                $_SESSION['juara'] = $nama;
+            }
+
+            echo "<p>Rata-rata tertinggi saat ini adalah: {$_SESSION['tertinggi']}</p>";
+            echo "<p>Siswa dengan rata-rata tertinggi adalah: {$_SESSION['juara']}</p>";
+
+            $_SESSION['loop']++; 
+        }
+    }
+  
+    ?>
+    </div>
+    
 </body>
 </html>
-
-<?php
-    if (isset($_POST['submit'])) {
-        $bilangan = $_POST['bil'];
-
-        $satuan = $bilangan % 10;
-        $puluhan = ( $bilangan / 10) % 10;
-        $ratusan = ( $bilangan / 100) % 10;
-
-        echo "<br>satuan: " . $satuan . "<br>","<br>puluhan: " . $puluhan . "<br>",
-        "<br>ratusan: " . $ratusan . "<br>";
-    }
-?>
--- 12209044-Hikmatunnisa Rangkuti/soal19.php
+++ 12209044-Hikmatunnisa Rangkuti/soal19.php
@@ -1,38 +1,103 @@
-@@ -0,0 +1,37 @@
-<?php
-    $suhu_fahrenheit;
-    $suhu_celcius;
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>suhu</title>
+    <title>Soal 19</title>
 </head>
 <body>
+    <!-- input -->
+    <h2>Perhitungan Keuntungan Bioskop</h2>
     <form action="" method="post">
-        <div style="display: flex;">
-            <label for="fahren">suhu fahrenheit :</label>
-            <input type="number" name="fahren" id="fahren">
-        </div>
-        <button type="submit" name="submit">Kirim!</button>
+        <table>
+            <tr>
+                <td> Tiket VIP</td>
+                <td>  :  </td>
+                <td><input type="number" name="vip" id="vip"></td>
+            </tr>
+            <tr>
+                <td>Tiket Eksekutif</td>
+                <td>  :  </td>
+                <td><input type="number" name="eksekutif" id="eksekutif"></td>
+            </tr>
+            <tr>
+                <td>Tiket Ekonomi</td>
+                <td>  :  </td>
+                <td><input type="number" name="ekonomi" id="ekonomi"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="submit" id="submit"></td>
+            </tr>
+        </table>
     </form>
-</body>
-</html>
-<?php
-    if (isset($_POST['submit'])) {
-        $suhu_fahrenheit = $_POST['fahren'];
-        $suhu_celcius = floor ($suhu_fahrenheit / 33.8);
 
-        if ($suhu_celcius > 300) {
-            echo "suhu: " . $suhu_celcius . "cc panas";
+    <?php
+    // proses
+    if (isset($_POST['submit'])) {
+        $tiket [] = $_POST['vip'];
+        $tiket [] = $_POST['eksekutif'];
+        $tiket [] = $_POST['ekonomi'];
+
+        $vip = $tiket[0];
+        $eksekutif = $tiket[1];
+        $ekonomi = $tiket[2];
+
+        if ($vip >=40) {
+            $keuntungan_vip = 25;
         }
-        else if ($suhu_celcius > 250) {
-            echo "suhu: " . $suhu_celcius . "cc dingin";
+        elseif ($vip >= 20 && $vip < 40) {
+            $keuntungan_vip= 15;
+        }
+        elseif ($vip >0 && $vip <20) {
+            $keuntungan_vip = 5;
         }
         else {
-            echo "suhu: " . $suhu_celcius . "cc normal";
+            $keuntungan_vip= 0;
         }
-    }
-?>
+
+        if ($eksekutif >= 40) {
+            $keuntungan_eksekutif = 20;
+        }
+        elseif ($eksekutif >= 20 && $eksekutif <40) {
+            $keuntungan_eksekutif = 10;
+        }
+        elseif ($eksekutif <20 &&  $eksekutif >0) {
+            $keuntungan_eksekutif = 2;
+        }
+        else{
+            $keuntungan_eksekutif = 0;
+        }
+
+        if ($ekonomi >0 && $ekonomi <=50) {
+            $keuntungan_ekonomi= 7;
+        }
+        else {
+            $keuntungan_ekonomi= 0;
+        }
+
+        $seluruh_keuntungan = $keuntungan_vip + $keuntungan_eksekutif + $keuntungan_ekonomi;
+        $total = array_sum($tiket);
+
+        //output
+        echo "<br>";
+        echo "Keuntungan Seluruh Kelas = $seluruh_keuntungan%";
+        echo "<br>";
+        echo "Keuntungan Kelas VIP = $keuntungan_vip%";
+        echo "<br>";
+        echo "Keuntungan Kelas Eksekutif = $keuntungan_eksekutif%";
+        echo "<br>";
+        echo "Keuntungan Kelas Ekonomi = $keuntungan_ekonomi%";
+        echo "<br>";
+
+        echo "<br>";
+        echo "Jumlah Seluruh Tiket yang Terjual = $total tiket dari 150 tiket";
+        echo "<br>";
+        echo "Jumlah Tiket VIP yang Terjual = $vip tiket dari 50 tiket";
+        echo "<br>";
+        echo "Jumlah Tiket Eksekutif yang Terjual = $eksekutif tiket dari 50 tiket";
+        echo "<br>";
+        echo "Jumlah Tiket Ekonomi yang Terjual = $ekonomi tiket dari 50 tiket";
+     }
+    ?>
+</body>
+</html>
