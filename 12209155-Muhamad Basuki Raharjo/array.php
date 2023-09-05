3c67c9cf64a9b521dd42cf2c873b7858a616df09
unknown <raharjobasuki471@gmail.com>
2023-09-05 08:13:28 +0700

    penambah kodingan

---
+++ 12209155-Muhamad Basuki Raharjo/array.php
@@ -0,0 +1,32 @@
+<?php
+// Array awal
+$nilaiSaya = array(80, 78, 72, 84, 92, 88);
+
+// Menemukan nilai tertinggi
+$nilaiTertinggi = max($nilaiSaya);
+
+// Menemukan nilai terendah
+$nilaiTerendah = min($nilaiSaya);
+
+echo "Nilai Saya: " . implode(', ', $nilaiSaya) . "<br>";
+echo "Dari keseluruhan nilai yang paling tinggi ialah: " . $nilaiTertinggi . "<br>";
+echo "Sedangkan nilai yang paling kecil: " . $nilaiTerendah . "<br>";
+
+// Mengurutkan dari yang terkecil
+sort($nilaiSaya);
+echo "Diurutkan dari yang terkecil: " . implode(', ', $nilaiSaya) . "<br>";
+
+// Mengurutkan dari yang terbesar
+rsort($nilaiSaya);
+echo " Diurutkan dari yang terbesar: " . implode(', ', $nilaiSaya) . "<br>";
+// rata rata
+$rataRata = array_sum($nilaiSaya) / count($nilaiSaya);
+echo "Apabila dibulatkan, rata-rata dari keseluruhan nilai saya menjadi: " . round($rataRata) . "<br>";
+
+
+// Mencari indeks nilai 72 dan menggantinya dengan 75
+$nilaiSaya = [80, 78, 72, 84, 92, 88];
+$nilaiSaya[2] = 75;
+echo " Setelah melakukan perbikan, nilai saya menjadi: " . implode(', ', $nilaiSaya) . "<br>";
+echo "Jadi nilai saya sekarang: " . implode(', ', $nilaiSaya) . "<br>";
+?>
