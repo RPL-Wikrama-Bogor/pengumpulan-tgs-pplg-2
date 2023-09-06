6dfa3812d82f5efcbc7ed25787a1d7bb0e3955a9
nazirsaputra26 <nazir.saputra00@gmail.com>
2023-09-06 13:28:28 +0700

    tugas

--- 12209309-Nazir Saputra/php/soal_8.php
+++ 12209309-Nazir Saputra/php/soal_8.php
@@ -1,4 +1,35 @@
 <?php
+$bilangan;
+$satuan;
+$puluhan;
+$ratusan;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="" method="post">
+<div style="display:flex;">
+            <label for="bilangan">masukan bilangan :</label>
+            <input type="number" id="bilangan" name="bilangan">
+        </div>
+        <button type="submit" name="submit">Kirim</button>
+    </form>
+</body>
+</html>
+<?php
+    if (isset($_POST['submit'])) {
 
+        $bilangan = $_POST['bilangan'];
 
+        $satuan= $bilangan % 10;
+        $puluhan =floor ($bilangan / 10) % 10;
+        $ratusan = floor ($bilangan / 100);
+
+        echo "<br> satuan:" . $satuan . "<br> puluhan:" . $puluhan . "<br> ratusan:" . $ratusan;
+    }
 ?>
