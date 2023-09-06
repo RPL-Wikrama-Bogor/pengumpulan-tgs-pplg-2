dfff05a1000c810f7f83d6e1ea7f138cd5f4ce14
Muhamad Rizki Hasan <muhamadrizkihasan@smkwikrama.sch.id>
2023-09-06 22:07:53 +0700

    Menambahkan tugas membuat menu menggunakan array

--- 12209191-Muhamad Rizki Hasan/arrayMenu.php
+++ 12209191-Muhamad Rizki Hasan/arrayMenu.php
@@ -34,7 +34,7 @@ $menus = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu</title>
+    <title>Menu Makanan dan Minuman</title>
     <style>
         body {
             margin: 2% 30% 0 30%;
