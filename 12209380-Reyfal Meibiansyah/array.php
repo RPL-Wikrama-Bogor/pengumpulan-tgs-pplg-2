130f2a4f0c5db0799ca2340083fbd6fb1cd6fd1e
Farid Hardiansyah <reyfalmeibiansyah@smkwikrama.sch.id>
2023-09-06 19:20:07 +0700

    menambahkan file array

---
+++ 12209380-Reyfal Meibiansyah/array.php
@@ -0,0 +1,42 @@
+<?php 
+$nilai = [80,78,72,84,92,88];
+$sumNilai;
+$rataNilai;
+
+
+
+echo "nilai saya: " . implode(",", $nilai) . "<br>";
+
+echo "Nilai tertinggi adalah: " . max($nilai) . "<br>" ;
+
+echo "Nilai terkecil adalah: " . min($nilai). "<br>";
+
+asort($nilai);
+
+echo "Urutan dari terkecil ke terbesar: " . implode("," , $nilai);
+ 
+echo "<br>";
+
+arsort($nilai);
+
+echo "Urutan dari terbesar ke terkecil: " . implode( "," , $nilai) ;
+
+
+echo "<br>";
+
+$sumNilai = array_sum($nilai);
+$rataNilai = $sumNilai / 6 ;
+
+echo "Rata-rata nilai saya: " . round($rataNilai) ;
+
+echo "<br>";
+
+$nilai[2] = 75;
+
+ksort($nilai);
+
+echo "setelah remedial nilai 72 sekarang menjadi 75, maka nilai saya menjadi: " . implode(",", $nilai) . "<br>";
+
+arsort($nilai);
+
+echo "sehingga sekarang urutan nilai terbesar saya adalah: " . implode( ",",$nilai) ;
