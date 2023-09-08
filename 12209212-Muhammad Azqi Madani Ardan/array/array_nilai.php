9ed9d6c79cc2f973c9e52a1687e82816562fd447
MuhammadAzqiMadaniArdan <muhammadazqimadaniardan@smkwikrama.sch.id>
2023-09-08 13:12:11 +0700

    update 2

--- 12209212-Muhammad Azqi Madani Ardan/array/array_nilai.php
+++ 12209212-Muhammad Azqi Madani Ardan/array/array_nilai.php
@@ -1,87 +1,61 @@
 <?php
 
-$listFilm= [
-    [
-        "judul" => "miracle in cell no.7",
-        "min-usia" => 15,
-        "harga" => 45000
-    ],
-    [
-        "judul" => "The invitation",
-        "min-usia" => 17,
-        "harga" => 35000
-    ],
-    [
-        "judul" => "luck",
-        "min-usia" => 7,
-        "harga" => 35000
-    ]
-];
+$nilai = [80,78,72,84,92,88];
 
+$nilai2 = $nilai;
+//arr menjadi string
+$nilaiString = implode(',',$nilai);
+echo "Nilai Saya: ".$nilaiString. "<br>";
+
+//nilai terbesar
+$nilaiMax =max($nilai);
+echo "Nilai Terbesar : ".$nilaiMax . "<br>";
+
+//nilai terkecil
+$nilaiMin = min($nilai);
+echo "Nilai Terkecil : ".$nilaiMin . "<br>";
+
+//urut dari terkecil
+sort($nilai2);
+echo "Urutan dari yang terkecil: ".implode(',',$nilai2)."<br>";
+
+//urut dari terbesar
+rsort($nilai2);
+echo "Urutan dari yang terbesar: ".implode(',',$nilai2)."<br>";
+
+
+//menghitung rata-rata
+$rata= array_sum($nilai)/ count($nilai);
+$bulat = round($rata);
+echo "Nilai Rata : ".$bulat . "<br>";
+
+
+
+// // $nilai[2] = 75;
+// echo "Saya melakukan perbaikan untuk nilai ".$nilaiMin . " Saya mendapat nilai : ".$nilai[2]." Jadi Nilai Saya Sekarang ".implode(',',$nilai)."<br>";
+
+$key = array_search(72,$nilai);
+if ($key !== false ){
+    $nilai[$key] = 75;
+    echo "Saya melakukan perbaikan untuk nilai ".$nilaiMin . " Saya mendapat nilai : ".$nilai[$key]." Jadi Nilai Saya Sekarang ".implode(',',$nilai)."<br>";
+
+}
+
+// $nilai = array_map(function($value) {
+//     return ($value == 72) ? 75 : $value;}, $nilai);
+    
+//     echo "Saya melakukan perbaikan untuk nilai ".$nilaiMin . " Saya mendapat nilai : "."75"." Jadi Nilai Saya Sekarang ".implode(',',$nilai)."<br>";
+
+//mencari dalam array (var,index,jumlah item yang diambil,nilai)
+// array_slice($nilai,$key1, 1,75);
+
+// if($nilai[2] == 72){
+//     $nilai[2] = 75;
+//     echo "Saya melakukan perbaikan untuk nilai ".$nilaiMin . " Saya mendapat nilai : ".$nilai[2]." Jadi Nilai Saya Sekarang ".implode(',',$nilai)."<br>";
+
+
+// }
+
+rsort($nilai);
+echo "Sekarang Urutan Nilai Terbesar : ".implode(',',$nilai);
 ?>
-
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <center>
-        <table>
-    <form action="" method="post">
-            <tr>
-                <td>Usia</td>
-                <td><input type="number" name="usia"></td>
-            </tr>
-            <tr>
-                <td>Film</td>
-                <td>
-                    <select name="judul" id="">
-                        <option hidden disabled selected>--pilih film</option>
-
-                        <?php
-                        foreach ($listFilm as $key => $film) {
-                        ?>
-
-                            <option value="<?= $key ?>"> <?= $film["judul"] ?></option>
-
-                        <?php
-
-                        };
-                        ?>
-
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <button type="submit" name="simpan" value="Simpan">Simpan</button>
-            </tr>
-        </form>
-    </table>
-    <?php
-
-    if (isset($_POST['simpan'])) {
-
-        // $post disamakan dengan atribut name
-        $usia = $_POST['usia'];
-        $filmId = $_POST['judul'];
-        $minUsia = $listFilm[$filmId]['min-usia'];
-        $harga = $listFilm[$filmId]['harga'];
-
-        if($usia >= $minUsia){
-            echo "<h2 style='color : green'>Silahkan untuk membayar sebesar Rp. ".number_format($harga,2,',','.')."</h2>";
-        }
-
-        else{
-            echo "<h2 style='color : red'>Usia belum cukup </h2> ";
-        }
-
-    }
-    ?>
-    </center>
-</body>
-
-</html>
