622c4ea76bec14d092af1badb8a1bd7c77dd92f3
rajib-n07 <115965334+rajib-n07@users.noreply.github.com>
2023-09-11 10:43:09 +0700

    Add files via upload

---
+++ 12209361-Rajib Narja Yas'Ad/soal13.php
@@ -0,0 +1,14 @@
+<?php 
+    for($i = 1; $i <=50; $i++) {
+        echo $i. '<br>';
+     }
+     for($i = 1; $i <=50; $i++) {
+        if ($i %2 == 1) {
+        echo $i. "bilangan genap <br>";
+        }
+     else {
+        echo $i. "bilangan ganjil <br>";
+     } }
+    //  true = 1
+    //  false = 0
+?>
