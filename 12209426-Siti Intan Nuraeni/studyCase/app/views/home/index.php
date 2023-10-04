c7198fab3f6b4c5f63a3d773894f7a6c071596d7
unknown <sitiintannuraeni@smkwikrama.sch.id>
2023-10-04 15:03:40 +0700

    penambahan kodingan mvc

---
+++ 12209426-Siti Intan Nuraeni/studyCase/app/views/home/index.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="jumbotron py-4 px-5">
+        <h2 class="display-4"><strong>Selamat datang!</strong></h2>
+        <hr class="my-4">
+       <div class="container">
+        <h3 class="fs-2 display-3 py-3 px-3">Inventaris Alat Pembelajaran</h3>
+            <ol class="list-group-numbered">
+                <h4 class="fs-4">Peraturan Peminjaman</h4>
+                <li class="list-group-item">Menemui petugas inventaris ketika akan meminjam dan mengembalikan barang</li>
+                <li class="list-group-item">Lama waktu peminjaman hanya 2 hari</li>
+                <li class="list-group-item">Menyerahkan kartu identitas ketika melakukan peminjam sebagai jaminan</li>
+            </ol>
+       </div>
+</div>
