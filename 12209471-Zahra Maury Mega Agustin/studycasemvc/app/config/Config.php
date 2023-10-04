6411567b105257826539cfe58d0ca97a04d0d49c
zackyseptian <septianzacky12@gmail.com>
2023-10-04 14:40:19 +0700

    tugaspinjaman

--- 12209471-Zahra Maury Mega Agustin/studycasemvc/app/config/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('BASE_URL', 'http://localhost/studycasemvc/public');
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'db_mvc_peminjaman');
-?>
