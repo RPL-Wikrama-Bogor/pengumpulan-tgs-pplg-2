e85ec38f2741a3ad56d93118277463cbb3f504fa
MuhammadDwiDarmaPutra <dwidarmaputra6@gmail.com>
2023-10-30 15:34:17 +0700

    penambahan kodingan

---
+++ 12209223-Muhammad Dwi Darma Putra/vue-project/src/App.vue
@@ -0,0 +1,108 @@
+<template>
+  {{ nama }}
+  {{ number }}
+  <div v-html="kelas"></div>
+  <p>---------------- data binding ------------------</p>
+  <button :disabled="nonaktif">button</button>
+  <h1 v-bind="property">KIW</h1>
+  <input :type="typeInput">
+  <p>---------------- data binding ------------------</p>
+  {{ count == 0 ? count + 1 : count + 2 }}
+  <p>---------------- v-if ------------------</p>
+
+  <button v-if="show">submit</button>
+  <button v-show="show">submit</button>
+
+  <div v-if="count == 1">
+    number 1
+  </div>
+  <div v-else-if="count == 2">
+    number 2
+  </div>
+  <div v-else>
+    lebih dari 2
+  </div>
+  <p>---------------- computed dan methods ------------------</p>
+  <input :type="typeInput">
+  <button @click="showPassword">show password</button>
+  <button @click="counterNumber">methods {{ counterButton }}</button>
+  <button @click="countComputed">Computed {{ numberComputed }}</button>
+  
+  <p>---------------- class dan style -----------------------</p>
+  <ul>
+    <li :class="{
+      active: isActive,
+      fs: isActive
+    }">
+    test
+    </li>
+  </ul>
+  <button @click="ubahWarna">ubah warna</button>
+  <p>------------------ rendering --------------------</p>
+  <ul>
+    <li v-for="(item,index) in daftarKelas">{{ index+1 }}{{ item }}</li>
+  </ul>
+  <p>-----------------v-model---------------------</p>
+  <input type="text" v-model="inputkelas">{{ inputkelas }}
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      daftarKelas: ['RPL 1', 'RPL 2', 'RPL 3'],
+      typeInput: 'password',
+      counterButton: 0,
+      numberComputed: 0,
+      show: false,
+      count: 3,
+      nama: 'Dwi',
+      number: 18,
+      kelas: '<h1>Kelas 11</h1>',
+      nonaktif: false,
+      property: {
+        id: 1,
+        class: 'color'
+      },
+      typeInput: 'password',
+      isActive: true
+    }
+    },
+      methods:{
+        counterNumber(){
+          this.counterButton += 1;
+        },
+        showPassword(){
+          if(this.typeInput=='password'){
+            this.typeInput ='text';
+          } else {
+            this.typeInput = 'password'
+          }
+        },
+        ubahWarna(){
+          if(this.isActive) {
+            this.isActive = false;
+          } else {
+            this.isActive = true;
+          }
+        }
+      },
+      computed:{
+      countComputed(){
+        this.numberComputed += 6;
+      }
+    },
+  }
+</script>
+
+<style>
+.color {
+  color: green;
+}
+.fs{
+  font-size: 20px;
+}
+.active{
+  color: blue;
+}
+</style>
