6ff3a5af7e8e9cd482545631d12e35c97a0e0782
Farid Hardiansyah <reyfalmeibiansyah@smkwikrama.sch.id>
2023-10-31 19:33:15 +0700

    Menambahkan tugas card component

---
+++ 12209380-Reyfal Meibiansyah/tugas_card/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <card v-for="(item,index) in cardContents" :key="index" :judul="item.title" :text="item.text" :image="item.image">
+  <small>created by: Reyfal</small>
+  </card>
+</template>
+
+<script>
+  import Card from '@/components/Card.vue';
+  export default {
+    components: {
+      Card,
+    },
+    data() {
+      return {
+      cardContents: [
+        {
+          title: 'cobaaaaaaaaaaaaaa',
+          text: '<p>aoiwjoidoawhodhwhaihdoiiiiiiwhioahoishdihsahdhwalksdjw</p>',
+          image: 'dogman.jpeg',
+        },
+        {
+          title:'biarinnnnnnnnnnnnnnnnn',
+          text: '<p>awkdaowdibbhdhhhhhhhhhhhhhhhkscugygahvscwgfcastcevc</p>',
+          image: 'lil nus nus.jpeg',
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>
