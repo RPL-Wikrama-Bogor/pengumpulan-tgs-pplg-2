665205c2b23232cc85cb28d29289de647f8c356b
unknown <muhammadyazidwiliadi@smkwikrama.sch.id>
2023-10-31 20:56:29 +0700

    study case vue

---
+++ 12209281-Muhammad Yazid Wiliadi/case-components/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <Card v-for="(item, index) in cardContents" :key="index" :judul="item.title" :text="item.text" :image="item.image">
+    <small>created by: @vlykz_</small>
+  </Card>
+</template>
+
+<script>
+// panggil file
+import Card from "@/components/Card.vue";
+export default {
+  // component apa saja yang akan ditampilkan
+  components: {
+    // definisi component agar bisa digunakan di template
+    Card,
+  },
+  data() {
+    return {
+      cardContents: [
+        {
+          title: "Cara Makan Mie :",
+          text: `<ul>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, esse!</li>
+                    <li>Lorem ipsum dolor sit amet.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+                </ul>`,
+          image: 'kuma.jpg',
+        },
+        {
+          title: "Cara Seduh Mie :",
+          text: `<ul>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, esse!</li>
+                    <li>Lorem ipsum dolor sit amet.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+                </ul>`,
+          image: 'peter.jpeg',
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>
