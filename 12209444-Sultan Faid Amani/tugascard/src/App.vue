ffc4088b453b9b8cd273c9d2c04ad6dc1cf29ea6
sultanfaid26 <edd.zacky@gmail.com>
2023-11-01 08:07:30 +0700

    Tugas Card

---
+++ 12209444-Sultan Faid Amani/tugascard/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <Card v-for="(item, index) in cardContents" :key="index" :judul="item.title" :text="item.text" :image="item.image">
+  <small>created by: Sultan</small>
+  </Card>
+</template>
+
+<script>
+import Card from '@/components/Card.vue';
+  export default{
+    //component apa saja yg akan di tampilkan
+    components: {
+      // definisi component agar bisa digunakan di template
+      Card,
+    },
+    //penyimpanan variable
+    data(){
+      return {
+      cardContents: [
+        {
+          title: 'Cara Menjadi Miskin No root',
+          text: `<ul>
+                  <li>Gak Kerja</li>
+                  <li>Gak Kerja</li>
+                  <li>Gak Kerja</li>
+                 </ul>`,  
+          image: 'malas.jpg'
+        },
+        {
+          title: 'Cara Menjadi Kaya No root',
+          text: `<ul>
+                  <li>Kerja</li>
+                  <li>Kerja</li>
+                  <li>Kerja</li>
+                 </ul>`,
+          image: 'paleojavanicus.jpeg'
+        },
+      ]
+    }
+  }
+}
+</script>
---
+++ 12209444-Sultan Faid Amani/tugascard/src/components/Card.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="wrap">
+    <div class="card">
+    <div class="img-wrap">
+        <img :src="'/src/assets/img/'+image" alt="">
+    </div>
+    <div class="text-wrap">
+        <h3>{{ judul }}</h3>
+        <div v-html="text"></div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        //mengambil data dari luar atau file lain
+        //nama props == nama bind attribute di componentnya tempat data dikirm
+        props: ['judul', 'text', 'image'],
+    }
+</script>
+
+<style scoped>
+.wrap{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.card{
+    display: flex;
+    width: 600px;
+    height: auto;
+    border-radius: 5px;
+    border: 1px solid black;
+        
+}
+.img-wrap{
+    width: 35%;
+    height: auto;
+    position: relative;
+}
+.text-wrap{
+    width: 56%;
+    height: 100%;
+    padding: 10px;
+}
+.img-wrap img {
+    width: 100%;
+    height: 100%;
+    
+}
+</style>
