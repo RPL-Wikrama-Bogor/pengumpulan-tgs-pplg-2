33c6082aa2230db659dd01be54545ea904e324d0
farhan26 <Mfarhanudin2647@gmail.com>
2023-11-16 12:54:14 +0700

    pengumpulan tugas

---
+++ 12209168-Muhamad farhanudin/vue-router/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <ul>
+    <li>
+      <!-- berfungsi mau kemana -->
+      <Router-Link :to="{name : 'Home'}">Home</Router-Link>
+      <Router-Link to="/About">About</Router-Link>
+    </li>
+  </ul>
+  <!-- komponen mana yg dipanggil -->
+  <router-view></router-view>
+</template>
