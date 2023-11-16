bf1d01aef9b944d39f076620e7e09ac07047f1e1
AtefAnggaMalikFadilah <atefanggamf@gmail.com>
2023-11-16 21:39:18 +0700

    materi industri

---
+++ 12208924-Atef Angga Malik Fadilah/compro-wikrama/src/pages/Home.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <Beranda :data="DataHome"></Beranda>
+        <Service :data="DataLayanan"></Service>
+        <Portofolio :data="DataPortofolio"></Portofolio>
+        <Blog :data="DataBlog"></Blog>
+    </div>
+</template>
+
+
+<script>
+    import Beranda from '@/components/beranda/Beranda.vue'
+    import Service from '@/components/beranda/Service.vue'
+    import Portofolio from '@/components/beranda/Portofolio.vue'
+    import Blog from '@/components/beranda/Blog.vue'
+    import { Get } from '@/Api/index.js' 
+    export default {
+        components: {
+            Beranda,
+            Service,
+            Portofolio,
+            Blog
+        },
+        data() {
+            return {
+                DataHome: [],
+                DataLayanan: [],
+                DataPortofolio: [],
+                DataBlog: []
+            }
+        },
+        async created() {
+            const response = await Get('http://127.0.0.1:9000/api/home')
+            this.DataHome = response.data;
+            const responseLayanan = await Get('http://127.0.0.1:9000/api/services')
+            this.DataLayanan = responseLayanan.data.data;
+            const responsePortofolio = await Get('http://127.0.0.1:9000/api/portfolio')
+            this.DataPortofolio = responsePortofolio.data.data;
+            const responseBlog = await Get('http://127.0.0.1:9000/api/blog')
+            this.DataBlog = responseBlog.data.data;
+        },
+    }
+</script>
